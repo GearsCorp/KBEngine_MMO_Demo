354bf878b320ca6de8083884987bea1072db4473
Cat <zw.ambitiouscat@gmail.com>
2019-10-09 23:48:45 +0800

    修改

--- Assets/Scripts/View/AccountView.cs
+++ Assets/Scripts/View/AccountView.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AccountView : ComponentObject
 {
+    private Transform _selectPanel;
+    private Transform _createPanel;
+
     public AccountView()
     {
+        _selectPanel = GameObject.Find("Canvas").transform.Find("AvatarSelectPanel");
+        _createPanel = GameObject.Find("Canvas").transform.Find("AvatarCreatePanel");
+        _selectPanel.Find("CreateAvatarButtion").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            _selectPanel.gameObject.SetActive(false);_createPanel.gameObject.SetActive(true);
+        });
+        _createPanel.Find("CreateAvatarButtion").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            
+        });
 
     }
 }
